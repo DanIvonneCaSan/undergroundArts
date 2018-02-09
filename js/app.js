234affcd9ef561acac28aed040d39ad09865ffdd
DanivonneCaSan <32868919+DanIvonneCaSan@users.noreply.github.com>
2018-02-09 01:46:02 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,4 +1,4 @@
- $(document).ready(function(e){
+ /*$(document).ready(function(e){
       $('.search-panel .dropdown-menu').find('a').click(function(e) {
   		e.preventDefault();
  		var param = $(this).attr("href").replace("#","");
@@ -6,7 +6,32 @@
  		$('.search-panel span#search_concept').text(concept);
   		$('.input-group #search_param').val(param);
   	});
-  });
+  }); */
+  function htmlbodyHeightUpdate(){
+		var height3 = $( window ).height()
+		var height1 = $('.nav').height()+50
+		height2 = $('.main').height()
+		if(height2 > height3){
+			$('html').height(Math.max(height1,height3,height2)+10);
+			$('body').height(Math.max(height1,height3,height2)+10);
+		}
+		else
+		{
+			$('html').height(Math.max(height1,height3,height2));
+			$('body').height(Math.max(height1,height3,height2));
+		}
+		
+	}
+	$(document).ready(function () {
+		htmlbodyHeightUpdate()
+		$( window ).resize(function() {
+			htmlbodyHeightUpdate()
+		});
+		$( window ).scroll(function() {
+			height2 = $('.main').height()
+  			htmlbodyHeightUpdate()
+		});
+	});
   
 // var database = firebase.database();
 // Initialize Firebase
