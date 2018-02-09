903a0d569e736493b4ae91f6db7e38fc76525bc6
DanivonneCaSan <32868919+DanIvonneCaSan@users.noreply.github.com>
2018-02-09 01:39:20 -0600

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,13 +1,12 @@
-// Función para eñ menú desplegable
  $(document).ready(function(e){
-   $('.search-panel .dropdown-menu').find('a').click(function(e) {
-    e.preventDefault();
-    var param = $(this).attr("href").replace("#","");
-    var concept = $(this).text();
-    $('.search-panel span#search_concept').text(concept);
-    $('.input-group #search_param').val(param);
- });
- });
+      $('.search-panel .dropdown-menu').find('a').click(function(e) {
+  		e.preventDefault();
+ 		var param = $(this).attr("href").replace("#","");
+ 		var concept = $(this).text();
+ 		$('.search-panel span#search_concept').text(concept);
+  		$('.input-group #search_param').val(param);
+  	});
+  });
   
 // var database = firebase.database();
 // Initialize Firebase
